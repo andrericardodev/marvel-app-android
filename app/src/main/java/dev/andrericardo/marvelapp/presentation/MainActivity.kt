c5b2944077ce59a64dac3ae51798a7ce93fcceea
Andre Ricardo <andrericardo.developer@outlook.com>
2023-07-28 22:19:49 -0300

    configure project modules

--- app/src/main/java/dev/andrericardo/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/dev/andrericardo/marvelapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package dev.andrericardo.marvelapp
+package dev.andrericardo.marvelapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.andrericardo.marvelapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
