510f9d6ab0fef4e94c1f2a6acb6f52d74887b9a6
Andre Ricardo <andrericardo.developer@outlook.com>
2023-09-04 22:54:40 -0300

    Config Dagger Hilt

--- app/src/main/java/dev/andrericardo/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/dev/andrericardo/marvelapp/presentation/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
 import dev.andrericardo.marvelapp.R
 import dev.andrericardo.marvelapp.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
